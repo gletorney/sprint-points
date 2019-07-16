748fc24272a856c091902497ce1fe6e6dcc385e8
Graham Letorney <gletorney@shutterstock.com>
2019-07-16 15:39:34 -0400

    GL

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class Header extends React.Component {
+
   
   handleLogout = () => {
     this.props.onLogout();
   }
+
+  handleTryMe = () => {
+    let i = 0;
+    let themes = [
+      "theme-1",
+      "theme-2",
+      "no-theme",
+    ];
+    let body = document.getElementById('appBody');
+    return () => {
+      i = (i + 1) % themes.length;      
+      body.setAttribute('class', themes[i]);
+    }
+  }
+
+  tryMe = this.handleTryMe();
 
   render() {
     const { myUser } = this.props;
@@ -15,7 +32,13 @@ class Header extends React.Component {
           <h1 className="pad-10">Sprint points</h1>
         </header>
         <div className="border-bottom-1-ccc pad-10">
-            Hello {myUser.name} <span onClick={this.handleLogout} className="color-blue cursor-pointer float-right">Logout</span>
+            Hello {myUser.name} 
+            <span onClick={this.handleLogout} className="color-blue cursor-pointer float-right">
+              Logout
+            </span>
+            <span onClick={this.tryMe} className="marg-right-20 color-blue cursor-pointer float-right">
+              Try me
+            </span>
         </div>
       </React.Fragment>
     )
